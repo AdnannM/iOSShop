2cabbd17ef047bffe8772943f08a0c290538729c
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-11-03 10:03:28 +0100

    WellcomeVC

--- iOSShop/Controllers/WellcomeViewController.swift
+++ iOSShop/Controllers/WellcomeViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class WellcomeViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var continueButton: UIButton! {
+        didSet {
+            continueButton.layer.cornerRadius = 20
+            continueButton.layer.borderColor = UIColor.white.cgColor
+            continueButton.layer.borderWidth = 2
+        }
+    }
+    
+    
+    // View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        
     }
 
 
