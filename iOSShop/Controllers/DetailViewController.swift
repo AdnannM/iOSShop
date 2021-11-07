2809e28b8df9cde36192d82b03afaaced2c50b29
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-11-07 19:05:52 +0100

    add DetailVC for order Items

---
+++ iOSShop/Controllers/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  iOSShop
+//
+//  Created by Adnann Muratovic on 07.11.21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    // View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
